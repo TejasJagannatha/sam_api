f2486f98d8e3f3941c82b33aeb59c56b631369dc
Tejas <tejasjagannatha@gmail.com>
2025-04-18 08:46:47 +0530

    working-project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # sam_api
-Please follow the readme file
+Please follow the readme file.
+
+<h3>Install requirements.txt by the command:</h3>
+
+pip install -r requirements.txt
+
+Get into the folder <b>sam_api</b>
+
+<b>Run the command: </b>
+
+ uvicorn capitals:app --reload
+
+<i>this will start the local server <i>
+
+
+
+<h2>Swagger Docs</h2>
+localhost/docs
+
+Hit the GET api, gives you the random country
+Guess via the POST api, gives you if its correct or returns with the correct capital.
+
+
--- capitals.py
+++ capitals.py
@@ -7,22 +7,31 @@ app= FastAPI()
 country_capitals= {}
 
 class Country(BaseModel):
-    name:str
+    capital:str
 
 @app.get('/randomcountry')
 def get_country():
+    global country_capitals
     results = requests.get("https://countriesnow.space/api/v0.1/countries/capital").json()
     result_list = results["data"]
     countries_list = [i['name'] for i in result_list]
     capitals_list= [i['capital'] for i in result_list]
     random_country= random.choice(countries_list)
-    c=[each for each in result_list if each['name'] == random_country][0]
-    print(result_list)
+    random_country_details=[each for each in result_list if each['name'] == random_country][0]
+    country_capitals= random_country_details
+    #print(country_capitals)
     return random_country
 
 @app.post('/country')
 def main(country: Country):
-
-    return f"hello {country.name}"
+    #print(random_country)
+    #print(country_capitals)
+    country_capital= country_capitals['capital'].lower().strip()
+    guessed_capital= country.capital.lower().strip()
+    #print(country_capital)
+    if country_capital == guessed_capital:
+        return f"The Guess is correct"
+    else:
+        return f"The Guess is Incorrect, correct capital: {country_capital}"
 
 
